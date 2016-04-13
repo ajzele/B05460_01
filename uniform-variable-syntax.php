ba79386af9be1c6045183b608ddae0d482dd2d20
Branko Ajzele <ajzele@gmail.com>
2016-04-13 21:36:12 +0200

    Update uniform-variable-syntax.php

--- uniform-variable-syntax.php
+++ uniform-variable-syntax.php
@@ -35,3 +35,40 @@ Foo::{$bar['baz']}()
 // PHP 7:
 // Calling a closure within an array in a static variable
 (Foo::$bar)['baz']()
+
+
+// Access a static property on a string class name
+// or object inside an array
+$foo['bar']::$baz;
+// Access a static property on a string class name or object
+// returned by a static method call on a string class name
+// or object
+$foo::bar()::$baz;
+// Call a static method on a string class or object returned by
+// an instance method call
+$foo->bar()::baz();
+
+
+// Call a callable returned by a function
+foo()();
+// Call a callable returned by an instance method
+$foo->bar()();
+// Call a callable returned by a static method
+Foo::bar()();
+// Call a callable return another callable
+$foo()();
+
+// Access an array key
+(expression)['foo'];
+// Access a property
+(expression)->foo;
+// Call a method
+(expression)->foo();
+// Access a static property
+(expression)::$foo;
+// Call a static method
+(expression)::foo();
+// Call a callable
+(expression)();
+// Access a character
+(expression){0};
